6e3950096912002f57a4642881a16b49c30b8b68
Lex Nelson <lexinelson597@gmail.com>
2021-08-10 14:28:38 -0400

    commit tree 0e53bdfcfc02775b5bd3cb6f54bf14901ebefcbd

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    sentence = self.split(" ")
+    sentence.map do |words|
+      words.delete "."
+      words.delete "?"
+      words.delete "!"
+      binding.pry
+    end
+    sentence.length
   end
+
+
 end
